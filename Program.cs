68bf09522b9d2c47b12bada02cd6880ba44bb13e
Keegan Farkas <keeganfarkas@gmail.com>
2024-04-22 16:42:43 -0500

    added Swagger so we can document/test endpoints easier

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// For Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "DSD_Capstone API";
+    config.Title = "DSD_Capstone API v1";
+    config.Version = "v1";
+});
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
@@ -11,7 +20,22 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+} else 
+{
+    // For Swagger
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "DSD_Capstone API";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
 }
+
+// Just to see how Swagger works
+app.MapGet("/hello", () => "Hello World!");
+app.MapGet("/helloyou", (string strName) => "Hello " + strName + "!");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
