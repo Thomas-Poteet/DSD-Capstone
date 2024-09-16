5a71e945fab7046a4ace51a03723a030342bdda8
Keegan Farkas <keeganfarkas@gmail.com>
2024-09-16 16:54:06 -0500

    mostly finished fe layout for invoice page on mobile

--- Pages/Invoice.cshtml.cs
+++ Pages/Invoice.cshtml.cs
@@ -6,10 +6,9 @@ namespace DSD_Capstone.Pages;
 public class InvoiceModel : PageModel
 {
     public List<string> Vendors { get; set; } = new List<string>();
-    public List<string> InvoiceNumbers { get; set; } = new List<string>();
     public void OnGet()
     {
-        // Lists needs to be replaced by db call. Most likely need
+        // List needs to be replaced by db call. Most likely need
         // .ToList() at the end of the call.
         Vendors = new List<string>
         {
@@ -29,31 +28,5 @@ public class InvoiceModel : PageModel
             "JBS USA",
             "FreshPoint"
         };
-
-        InvoiceNumbers = new List<string>
-        {
-            "INV001234",
-            "00056789",
-            "A123456",
-            "INV2024001",
-            "B0023456",
-            "00789012",
-            "C789123",
-            "00012345",
-            "INV100987",
-            "XYZ123456",
-            "INV2023007",
-            "D0012345",
-            "00345678",
-            "AB987654",
-            "INV009876",
-            "E0005678",
-            "F0001234",
-            "00456789",
-            "INV2024005",
-            "G0012345",
-            "123456"
-        };
-
     }
 }
