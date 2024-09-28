9db021ada41807fd2763525d1dee22e4ad2b703d
Thomas-Poteet <tjpoteet42@tntech.edu>
2024-09-27 23:21:11 -0500

    Added Vendors to Dashboard

--- Pages/Dashboard.cshtml.cs
+++ Pages/Dashboard.cshtml.cs
@@ -1,19 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 
 namespace DSD_Capstone.Pages;
 
 public class DashboardModel : PageModel
 {
-    private readonly ILogger<DashboardModel> _logger;
-
-    public DashboardModel(ILogger<DashboardModel> logger)
+    private readonly MyDbContext vendors;
+    public DashboardModel(MyDbContext context)
+        {
+            vendors = context;
+        }
+    public List<string> Vendors { get; set; } = new List<string>();
+    public async Task OnGetAsync()
     {
-        _logger = logger;
+        // Query the database to fill the Vendors list
+        Vendors = await vendors.Vendors
+            .Select(v => v.name)
+            .ToListAsync();
     }
-
-    public void OnGet()
-    {
-    }
+    
 }
 
